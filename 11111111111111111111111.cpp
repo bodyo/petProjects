622030165da968afa32dc18a18d0a12b72978665
bodyo <djei0967@gmail.com>
2017-01-22 19:21:11 +0200

    test_task

---
+++ 11111111111111111111111.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+int ato(const char *srt)
+{
+    int number = 1;
+    int num = 0;
+    int sum = 0;
+    int size = 0;
+    for(int i = 0; srt[i+1] != '\0'; i++)
+        size++;
+    cout << size<<endl;
+    for(int i = size; i >= 0; i--)
+    {
+        num = srt[size-i] - '0';
+        for(int j = 0; j < i; j++)
+            number*=10;
+        number*=num;
+        sum+=number;
+        number = 1;
+    }
+    return sum;
+}
+
+int ato1(const char *srt)
+{
+    int num = 0;
+    for(int i = 0; srt[i] != '\0'; i++)
+    {
+        num *= 10;
+        num += srt[i] - '0';
+    }
+    return num;
+}
+
+
+int atoo(const char *srt)
+{
+    int num = 0;
+    for(int i = 0; srt[i] != '\0'; i++)
+        num = num * 10 + (srt[i] - '0');
+    return num;
+}
+class A
+{
+public:
+    virtual void foo()
+    {
+        cout << "A";
+    }
+};
+class B: public A
+{
+private:
+    virtual void foo()
+    {
+        cout << "B";
+    }
+};
+
+
+int main()
+{
+    A *temp = new B;
+    //ano temp;
+    temp->foo();
+    //int (*foo)(const char*) = atoo;
+    //char *et = "2004";
+    //cout << foo(et) << endl << ato1(et);
+}
